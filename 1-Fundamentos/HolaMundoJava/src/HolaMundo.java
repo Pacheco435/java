d9df92991792bab8d6a8de17957bb2f707c07db4
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-01-15 12:09:50 -0500

    PRIMER CODIGO

--- 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -1,14 +1,34 @@
 //Mi clase en Java
 public class HolaMundo {
-       //Definimos la variable 
     public static void main(String args[]){
+        //Definimos la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
        // Modificamos el valor de la variable 
        miVariableEntera = 5 ;
         System.out.println(miVariableEntera);
-     }
-    
+     
+      String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        miVariableCadena= "Adios" ;
+        System.out.println(miVariableCadena);
+        
+        //var - inferencia de tipos en java
+        
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+        var miVariableCadena2 = "Nueva Cadena";
+         System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        
+         //Valores permitidos en el nonbre de variables 
+         var miVariable = 1;
+         var _miVariable = 2;
+         var $miVariable = 3;
+         // var áVariable =10 ,  no se recomienda utilizar
+         
+         
+   
+              }
     
 }
 
