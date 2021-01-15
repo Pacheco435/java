e2e7045275e2a470067ff96958f47252206be6df
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-01-15 15:25:14 -0500

    METODOS DE ESCRITURAS

--- 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -3,16 +3,12 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var usuario = "Juan";
-        var titulo = "Ingeniero";
-        var union = titulo + " " + usuario;
-        System.out.println("union = " + union);
-        var i = 3;
-        var j = 4;
-        System.out.println (i + j);// se realiza la suma de numeros 
-        System.out.println(i + j + usuario);// Evaluacion de izquierdaa a derecha relaiza suma 
-        System.out.println(usuario + i + j );// contexto cadena todo lo toma como cadena 
-        System.out.println(usuario + (i + j) );// modifica la prioridad en la evaluacion primero se hace  la suma 
+        var nombre = "Karla";
+        System.out.println("Nueva Linea:\n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "'");
+        System.out.println("Comilla doble:\""+ nombre + "\"" );
     }
 
 }
