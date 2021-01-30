f45b85b4908526263852852e1ac6126f09f6b953
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-01-30 14:00:09 -0500

    OPERADORES EN JAVA

--- 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ 1-Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -4,187 +4,181 @@ import java.util.Scanner;
 //Mi clase en Java
 
 public class HolaMundo {
-    
-
+   
     public static void main(String args[]) {
-        tarea();       
+        precedenciaDeOperadores();       
     }
-    public static void ejemploConcatena1(){
-        System.out.println("Escribe tu nombre:");
-        Scanner consola = new Scanner (System.in);
-        var usuario = consola.nextLine();
-        System.out.println("usuario = " + usuario);
-        System.out.println("Escribe el titulo");
-        var titulo = consola.nextLine() ;
-        System.out.println("Resultados: " + titulo + " " + usuario);
-        System.out.println("titulo");
+    public static void operadoresAritmeticos(){
+        int  a=3, b=2;
+        var resultados = a + b;
+        System.out.println("resultado = " + resultados);
         
+        resultados = a-b;
+        System.out.println("resultado =" + resultados);
         
+        resultados = a*b;
+        System.out.println("resultados = " + resultados);
+        
+        var resultados2 = 3.0/b;
+        System.out.println("resultado de division = " + resultados2);
+        
+        resultados = a%b;
+        System.out.println("resultados = " + resultados);
+        
+        if( a % 2 == 0)
+        System.out.println("es numero par");
+         else 
+        System.out.println("es numero impar");         
     }
-    
-    public static void ejemploConcatena2(){
-        Scanner consola = new Scanner (System.in);
-        var titulo = consola.nextLine();
-        System.out.println("titulo =" + titulo);
-        System.out.println("Escribe el usuario");
-        var usuario = consola.nextLine();
-        System.out.println(titulo + " " + "fue escrito por" + " " + usuario );
-        
-    }
-    
-    public static void byteAndShortIntLong(){
-        // TIPOS PRIMITIVOS ENTEROS:byte, short,int,long
-        byte numerobyte = (byte)129;
-        System.out.println("valor bytes" + numerobyte);
-        System.out.println("valor minimo del byte:" + Byte.MIN_VALUE);
-        System.out.println("valor maximo del byte:" + Byte.MAX_VALUE);
-        
-        short numeroShort = 10;
-        System.out.println("numeroShort = "+ numeroShort);
-        System.out.println("valor minimo del byte" + Short.MIN_VALUE);
-        System.out.println("valor maximo del byte" + Short.MAX_VALUE);
-        
-        int numeroInt = (int)2147483648L;
-        System.out.println("numeroInt =" + numeroInt);
-        System.out.println("valor minimo :" + Integer.MIN_VALUE);
-        System.out.println("valor maxima short :" + Integer.MAX_VALUE);
-        
-        long numeroLong =9223372036854775807L;
-        System.out.println("numeroLong =" + numeroInt);
-        System.out.println("valor minimo long :" + Long.MIN_VALUE);
-        System.out.println("valor maxima long :" + Long.MAX_VALUE);
-        
-        
-    }
-    
-    public static void primitivosFlotantes(){
-        /* 
-        tipos primitivos de tipo flotante: Float(32bits) y double (64bits)
-        */
-        float numeroFloat = (float)3.4028235E38D;
-        System.out.println("numeroFloat =" + numeroFloat);
-        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);
-        System.out.println("Valor maximo tipo float: " + Float.MAX_VALUE);
-        
-        double numeroDouble = 10;
-        System.out.println("numeroDouble = " + numeroDouble);
-        System.out.println("Valor minimo tipo double: " + Double.MIN_VALUE);
-        System.out.println("Valor maximo tipo double " + Double.MAX_VALUE);
-        
-    
-    }
-    
-    public static void tiposconVaryPrimitivos (){
-      var numeroEntero = 10;
-        System.out.println("nuemroEntero = " + numeroEntero);
-        
-        var numeroDouble = 10.0;
-        System.out.println("numerDouble = " + numeroDouble);
-        
-        var numeroFloat = 10.0F;
-        System.out.println("numeroFloat =" + numeroFloat);
-    }
-    
-    public static void tipoCharp (){
-        char miCaracter = 'a';
-        System.out.println("miCaracter = " + miCaracter);
-        
-        char varChar = '\u0021';
-        System.out.println("varChar = " + varChar);
-        
-        char varCharDecimal = 33;
-        System.out.println("varCharDecimal = " + varCharDecimal);
-        
-        char varCharSimbolo = '!';
-        System.out.println("varCharSimbolo = " + varCharSimbolo);
-        
-        //utilizando var 
-        
-        var varChar1 = '\u0021';
-        System.out.println("varChar1 = " + varChar1);
-        
-        var varCharDecimal2 = 33;
-        System.out.println("varCharDecimal2 = " + varCharDecimal2);
-        
-        var  varCharSimbolo3 = '!';
-        System.out.println("varCharSimbolo3 = " + varCharSimbolo3);
-        
-        int variableEnteraSimbolo = '!';
-        System.out.println("variableEnetraSimbolo = " + variableEnteraSimbolo);
-        
-        int letra = 'a';
-        System.out.println("letra = " + letra);
-    }
-    
-    public static void tipoBoolean (){
-        // tipo bandera 
-        boolean varBoolean = true;
-        System.out.println("varBoolean = " + varBoolean);
-        
-        if(varBoolean == true){
-            System.out.println("La bandera es verdadera");
-        }
-        else {
-            System.out.println("La bandera es falsa");
-        }
-        
-        var edad = 10;
-        //var esAdulto = edad >= 18;
-        if(edad >= 18){
-            System.out.println("Eres mayor de edad");
-        }
-        else{
-            System.out.println("no eres mayor de edad");
-        }
-        }
-        
-    public static void conversionTiposPrimitivos (){
-        // convertir tipo string a un tipo int 
-        var edad = Integer.parseInt("20");
-        //var edad = "20"
-        System.out.println("edad = " + (edad + 1 ));
-        
-        var valorPI = Double.parseDouble("3.1416");
-        System.out.println("valorPI = " + valorPI);
-        
-        //PEDIR VALOR 
-        var consola = new Scanner(System.in);
-//        System.out.println("Proporciona tu edad");
-//        edad = Integer.parseInt(consola.nextLine() );
-//        System.out.println("edad = " + edad);
-        
-        var edadTexto = String.valueOf(10);
-        System.out.println("edadTexto = " + edadTexto);
-        
-        var caracter = "hola".charAt(1);
-        System.out.println("caracter = " + caracter);
-        
-        System.out.println("proporciona un caracter");
-        caracter= consola.nextLine().charAt(0);
-        System.out.println("caracter = "+ caracter);
-        
-        
-        
-        
-    } 
-    
-    public static void tarea (){
+    public static void tareaRectangulo (){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Proporciona el nombre:");
-        String nombre = scanner.nextLine();
-        System.out.println("Proporciona el id:");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.println("Proporciona el precio:");
-        double precio = Double.parseDouble(scanner.nextLine());
-        System.out.println("Proporciona el envio gratuito:");
-        boolean envioGratuito = Boolean.parseBoolean(scanner.nextLine());
- 
-        System.out.println(nombre + " #" + id);
-        System.out.println("Precio: $" + precio);
-        System.out.println("Envio Gratuito: " + envioGratuito);
+        System.out.println("Proporciona el alto");
+        int alto = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el ancho");
+        int ancho = Integer.parseInt(scanner.nextLine());
+        int area = alto * ancho;
+        int perimetro = (alto + ancho)*2;
+        System.out.println("Area: " + area);
+        System.out.println("perimetro: " + perimetro);
+    }
+    public static void operadoresdeAsignacion (){
+    int a=3 , b=2;
+    int c= a + 5 - b;
+        System.out.println("c=" + c);
         
+        a += 1;// a =a+1 se incrementa en uno 
+        System.out.println("a=" + a);
+        
+        a +=3;
+        System.out.println("a=" + a);
+        
+        a -=2;
+        System.out.println("a=" + a );
+    }
+    public static void operadoresUnarios (){
+    var a = 3;
+    var b = -a;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        var c = true;
+        var d = !c;
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
+        
+        // incremento
+        //1.preincremento( simbolo antes de la variable)
+        var e =4;
+        var f = ++e; // primero se incrementa la variable y despues se usa su valor ;
+        System.out.println("e=" + e);
+        System.out.println("f=" + f);
+        //2. post incremento (simbolo despues de la variable
+        var g = 5;
+        var h = g++;//primero se utiliza el valor de la varible y luego se incrementa 
+        System.out.println("g=" + g);//teniamos pendiente un incremento
+        System.out.println("h=" + h);
+        
+        //decremento
+        var i=2;
+        var j= --i;
+        System.out.println("i=" + i);
+        System.out.println("j=" + j);
+        
+        //pst decremento
+        var k=4;
+        var l= k--;
+        System.out.println("k=" + k);
+        System.out.println("l"+ l);
+  
+    }
+    public static void operadoresdeIgualdadyRelacionales(){
+    var a = 3;
+    var b = 2;
+    var c = (a == b);
+        System.out.println("c=" + c);
+        
+    var d = a != b;
+        System.out.println("d=" + d);
+        
+        var cadena ="hola";
+        var cadena2 ="hola";
+        var e = cadena == cadena2;
+        System.out.println("e=" + e);
+        
+        var f= cadena.equals(cadena2);//comparamos contenidos de cadenas
+        System.out.println("f=" + f);
+        
+        //operadores relacionales 
+         var g = a>=b;//mayor que> o el mayor o igual >=
+         System.out.println("g=" + g);
+         
+         if (a % 2 == 0) { //aqui no lleva punto y coma
+         System.out.println("es numero par");
+          } else {
+         System.out.println("es numero impar");
+            }
+    }
+    public static void operadoresCondicionale(){
+        // USO DE LAS CONDICIONALES "Y" "O"
+    var a = 10;
+    var valorMinimo = 0;
+    var valorMaximo = 10;
+    
+    var resultado = a >= 0 && a <= 10; //es la condicional "y"
+    if(resultado)
+    {
+        System.out.println("Dentro de rango");
+    }
+    else
+    {
+        System.out.println("fuera de rango");
+    }
+    
+    var vacaciones = false ;
+    var diaDescanso = true;
+    
+    if(vacaciones || diaDescanso) //es la condicional "o"
+    {
+        System.out.println("Padre puede asistir al juego del hijo");
+    }
+    else
+    {
+        System.out.println("El padre esta ocupado");
+    }
+    
+    }
+    public static void operadorTernario (){
+    var resultado = (1>2) ? "verdad" : "falso" ;
+        System.out.println("resultado= " + resultado);
+    
+        var numero = 8;
+        resultado = (numero % 2 == 0) ? "numero par" : "numero impar";
+        System.out.println("resultado= " + resultado);
+    }
+    public static void tareaconRangos (){
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el numero1");
+        int numero1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el numero2");
+        int numero2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("El numero mayor es:");
+        System.out.println(numero1>numero2 ? numero1 : numero2);
+    }
+    public static void precedenciaDeOperadores(){
+    var x = 5;
+    var y = 10;
+    var z = ++x + y--;
+        System.out.println("x =" + x);
+        System.out.println("y =" + y);
+        System.out.println("z =" + z);
+        
+        var resultado = 4 + 5 *6 / 3;//4 + ((5*6)/3)
+        System.out.println("resultado:" + resultado);//14
+        resultado = (5+4)*6/3;
+        System.out.println("resultado:" + resultado);
+    
     }
     }
+    
     
 
     
