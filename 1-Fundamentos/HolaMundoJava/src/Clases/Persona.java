354ff8c77091e539c59916c967067bc140aedcd7
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-02-06 16:56:52 -0500

    2.CREACION DE CLASES Y OBJETOS

---
+++ 1-Fundamentos/HolaMundoJava/src/Clases/Persona.java
@@ -0,0 +1,11 @@
+package Clases;
+public class Persona {
+  String nombre;
+  String apellido;
+
+public void DesplegarInformacion(){
+    System.out.println("Nombre:" + nombre);
+    System.out.println("Apellido:"  + apellido);  
+}
+}
+
