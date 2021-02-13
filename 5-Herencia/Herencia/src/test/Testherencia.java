ae29ec1f1ea826791917d72c74e089776e5a33c5
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-02-13 13:50:01 -0500

    7-Herencia, sobrecarga de constructores

---
+++ 5-Herencia/Herencia/src/test/Testherencia.java
@@ -0,0 +1,20 @@
+
+package test;
+
+import dominio.Cliente;
+import dominio.Empleado;
+import dominio.Persona;
+import java.util.Date;
+import java.util.Scanner;
+public class Testherencia {
+    public static void main(String[] args) {
+       Empleado empleado1 = new Empleado("Juan" ,5000.0);
+       System.out.println("empleado1: " + empleado1);
+        //var fecha = new Date();
+        //Cliente cliente1 = new Cliente(fecha, true , "Karla" , 'F', 28 , "LA MERCED NRO 14");
+        //System.out.println("cliente =" + cliente1);
+        
+        //Persona persona1 = new Persona();
+        
+    }
+}
