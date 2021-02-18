09c4a7ab5f26c0129021c6fd2077f42151310439
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-02-18 16:05:52 -0500

    19 - Boxing UnBoxing

---
+++ 17-AutoBoxingUnBoxing/src/test/TestAutoBoxingUnBoxing.java
@@ -0,0 +1,22 @@
+package test;
+
+public class TestAutoBoxingUnBoxing {
+    public static void main(String[] args) {
+        //Clases Envolventes de tipos primitivos
+        /*
+        int = Integer
+        long = Long
+        float = Float
+        double = Double
+        */
+        //autoboxing convierte la literal de tipo entero a tipo object
+        Integer  entero =10;
+        System.out.println("entero = " + entero);
+        System.out.println("entero double = " + entero.doubleValue());
+        //el unboxing recupera del metodo autoboxing 
+        int entero2 = entero;
+        System.out.println("entero2 = " + entero2);
+        
+    }
+    
+}
