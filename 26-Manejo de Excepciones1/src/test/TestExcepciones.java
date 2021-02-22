33a41b6b0224c6c3cf47634ab840324755af5732
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-02-22 10:00:04 -0500

    28 - Manejo de excepciones

---
+++ 26-Manejo de Excepciones1/src/test/TestExcepciones.java
@@ -0,0 +1,26 @@
+package test;
+
+import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try{
+          resultado = division(10, 0);
+        }catch(OperacionExcepcion e){
+            System.out.println("Ocurrio un error de tipo operacion de ecepcion"); 
+             System.out.println(e.getMessage());
+        }
+        
+        catch(Exception e){
+            System.out.println("ocurrio un error de tipo exception");
+        //e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Se reviso la division entre 0");
+    }
+        System.out.println("resultado =  " + resultado);
+    }
+}
