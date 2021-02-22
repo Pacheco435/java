f9a31f97e27270b693716a48270f9f2a15417192
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-02-22 12:25:59 -0500

    31-Api Colecciones Genericas(set, lit, map)

---
+++ 29-ApiColeccionesGenericas/src/Test/TestColeccionesGenericas.java
@@ -0,0 +1,54 @@
+package Test;
+
+import java.util.*;
+
+public class TestColeccionesGenericas {
+    public static void main(String[] args) {
+        List <String> miLista = new ArrayList<>();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miercoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Sabado");
+        miLista.add("Domingo");
+        miLista.add("Domingo");
+        String elemento = miLista.get(0);
+     //   System.out.println("elemento = " + elemento);
+     // imprimir(miLista);
+        
+  
+        Set<String> miSet = new HashSet<>();
+        miSet.add("Lunes");
+        miSet.add("Martes");
+        miSet.add("Miercoles");
+        miSet.add("Jueves");
+        miSet.add("Viernes");
+        miSet.add("Sabado");
+        miSet.add("Domingo");
+        miSet.add("Domingo");
+        
+      // imprimir(miSet);
+       
+       Map<String, String> miMapa = new HashMap();
+       miMapa.put("valor1", "Juan");
+       miMapa.put("valor2", "Karla");
+       miMapa.put("valor3", "Rosario");
+       miMapa.put("valor3", "Carlos");
+       
+       String elementoMapa = miMapa.get("valor3");
+       //System.out.println("elementoMapa = " + elementoMapa);
+        imprimir(miMapa.keySet());
+        imprimir(miMapa.values());
+    }
+    public static void imprimir(Collection<String> colection){
+    
+//    colection.forEach(elemento -> {
+//        System.out.println("elemento = " + elemento);
+//    });
+    for(String elemento: colection){
+        System.out.println("elemento = " + elemento);
+    }
+    
+    }
+}
