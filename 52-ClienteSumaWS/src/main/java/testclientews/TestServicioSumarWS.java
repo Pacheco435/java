90f067d1c75beb8678e579d92e602effacfd9833
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-03-29 17:54:54 -0500

    52-Clientes Suma WS consume el servicio del proyecto 51

---
+++ 52-ClienteSumaWS/src/main/java/testclientews/TestServicioSumarWS.java
@@ -0,0 +1,19 @@
+package testclientews;
+
+import clientews.servicio.ServicioSumarImplService;
+import clientews.servicio.ServicioSumarWS;
+
+public class TestServicioSumarWS {
+
+    public static void main(String[] args) {
+        ServicioSumarWS servicioSumar = new ServicioSumarImplService().getServicioSumarImplPort();
+        System.out.println("Ejecutamos Servicio sumar WS");
+        int x = 6;
+        int y = 3;
+        int resultado = servicioSumar.sumar(x, y);
+        System.out.println("Resultado: " + resultado);
+        System.out.println("Fin de servicio sumar WS");
+        
+    }
+    
+}
