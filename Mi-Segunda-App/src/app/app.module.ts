10b9d430112386e6d43609e9571c3409d1eb9450
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-06-13 12:38:46 -0500

    Mi segunda app

---
+++ Mi-Segunda-App/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { PersonasComponent } from './personas/personas.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent, PersonasComponent, PersonaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
