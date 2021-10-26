04f4dce87cbd991ce9de75a90c50a897c138827d
pedraza arturo <arturopacheco_industrial@hotmail.com>
2021-10-26 13:36:51 -0500

    2da version

--- Mi-Segunda-App/src/app/personas/personas.component.ts
+++ Mi-Segunda-App/src/app/personas/personas.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
+  // tslint:disable-next-line: whitespace
   styleUrls:['./personas.component.css']
 })
 export class PersonasComponent{
   deshabilitar = false;
-  mensaje = 'No se ha agregado ninguna persona';
+  mensaje = '';
   titulo = 'Licenciado';
+  // tslint:disable-next-line: typedef
+  mostrar = false;
+
+  // tslint:disable-next-line: typedef
   agregarPersona(){
+    this.mostrar = true;
     this.mensaje = 'Persona agregada';
   }
 
